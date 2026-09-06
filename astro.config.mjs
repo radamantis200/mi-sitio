551ef3c9a2558e8ec509dc19785989ae3ebd2195
Jefry Sanchez <sanchezmorajeffrey@yahoo.com>
2026-09-06 16:37:10 -0600

    fix: corregir ubicación de inlineStylesheets para no romper Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
@@ -6,10 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  build: {
+    inlineStylesheets: 'always'
+  },
   vite: {
-    plugins: [tailwindcss()],
-    build: {
-      inlineStylesheets: 'always' // <-- ¡Agrega esta línea!
-    }
+    plugins: [tailwindcss()]
   }
 });
